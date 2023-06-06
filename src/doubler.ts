3d81a544ab204fe6d78ec14dc9c9febdd41d875f
Mike Harris <mharris717@gmail.com>
2023-06-06 17:16:21 -0400

    patch

--- src/doubler.ts
+++ src/doubler.ts
@@ -1,10 +1,16 @@
+
 export class Doubler {
   double(n: number): number {
     return n * 2;
+  }
+
+  triple(n: number) : number {
+    return n * 3;
   }
 }
 
 function main() {
   const d = new Doubler();
   console.log(d.double(7));
+  console.log(d.triple(7))
 }

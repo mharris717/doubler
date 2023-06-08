c202a8465e685ccd8d52a0f258fe6e6ebc040c90
Mike Harris <mharris717@gmail.com>
2023-06-08 19:37:27 -0400

    patch

--- src/doubler.test.ts
+++ src/doubler.test.ts
@@ -17,4 +17,13 @@ describe("Doubler", () => {
     // Assert
     expect(result).toBe(6);
   });
+  it("should triple 4 to 12", () => {
+    // Arrange
+    const d = new Doubler();
+    // Act
+    const result = d.triple(4);
+    // Assert
+    expect(result).toBe(12);
+  });
 });
+

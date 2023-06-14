f0e0a246232ab046deaf26e2c420b5b71f20cc95
Mike Harris <mharris717@gmail.com>
2023-06-14 19:03:51 -0400

    patch

--- src/doubler.test.ts
+++ src/doubler.test.ts
@@ -1,20 +1,8 @@
-import { Doubler } from "./doubler";
-
-describe("Doubler", () => {
-  it("should double 2 to 4", () => {
-    // Arrange
-    const d = new Doubler();
-    // Act
-    const result = d.double(2);
-    // Assert
-    expect(result).toBe(4);
-  });
-  it("should double 3 to 6", () => {
-    // Arrange
-    const d = new Doubler();
-    // Act
-    const result = d.double(3);
-    // Assert
-    expect(result).toBe(6);
-  });
+it("should triple 4 to 12", () => {
+  // Arrange
+  const d = new Doubler();
+  // Act
+  const result = d.triple(4);
+  // Assert
+  expect(result).toBe(12);
 });

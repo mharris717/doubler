00507b138e470c983bbbbe65e242fa10d8273f97
Mike Harris <mharris717@gmail.com>
2023-06-16 21:11:19 -0400

    patch

--- src/doubler.test.ts
+++ src/doubler.test.ts
@@ -17,6 +17,20 @@ describe("Doubler", () => {
     // Assert
     expect(result).toBe(6);
   });
+  it("should quadruple 2 to 8", () => {
+    // Arrange
+    const d = new Doubler();
+    // Act
+    const result = d.quadruple(2);
+    // Assert
+    expect(result).toBe(8);
+  });
+  it("should throw an error if quadruple method does not exist", () => {
+    // Arrange
+    const d = new Doubler();
+    // Act and Assert
+    expect(() => d.quadruple(2)).toThrow();
+  });
 });
 
 const foobar = `123`;

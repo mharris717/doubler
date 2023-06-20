a8604bed669873a3d2656280919ca2006e6fb595
Mike Harris <mharris717@gmail.com>
2023-06-20 13:57:19 -0400

    patch

--- src/doubler.ts
+++ src/doubler.ts
@@ -1,15 +1,19 @@
 export class Doubler {
-  double(n: number): number {
-    return n * 2;
+  double(n: string): number {
+    let num = parseInt(n);
+    if (isNaN(num)) { return 0; }
+    return num * 2;
   }
-  quadruple(n: number): number {
-    return n * 4;
+  quadruple(n: string): number {
+    let num = parseInt(n);
+    if (isNaN(num)) { return 0; }
+    return num * 4;
   }
 }
 
 function main() {
   const d = new Doubler();
-  console.log(d.double(7));
+  console.log(d.double('7'));
 }
 
 const foobar = `123`;
